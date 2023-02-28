c1885ca6247df064e25b658e54347d445675e3b0
hgko1207 <khkkhk1207@gmail.com>
2023-02-28 14:46:33 +0900

    Catch All

--- nextjs-intro/pages/index.js
+++ nextjs-intro/pages/index.js
@@ -5,15 +5,7 @@ import { useRouter } from 'next/router';
 export default function Home({ results }) {
   const router = useRouter();
   const onClick = (id, title) => {
-    router.push(
-      {
-        pathname: `/movies/${id}`,
-        query: {
-          title,
-        },
-      },
-      `/movies/${id}`
-    );
+    router.push(`/movies/${title}/${id}`);
   };
   return (
     <div className="container">
@@ -21,15 +13,7 @@ export default function Home({ results }) {
       {results?.map((movie) => (
         <div onClick={() => onClick(movie.id, movie.original_title)} className="movie" key={movie.id}>
           <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
-          <Link
-            href={{
-              pathname: `/movies/${movie.id}`,
-              query: {
-                title: movie.original_title,
-              },
-            }}
-            as={`/movies/${movie.id}`}
-          >
+          <Link href={`/movies/${movie.original_title}/${movie.id}`}>
             <h4>{movie.original_title}</h4>
           </Link>
         </div>
