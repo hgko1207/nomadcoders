4e1a87a170b6c0355fdc08471473e93aec8bb8b5
khkkhk1207 <khkkhk1207@gmail.com>
2023-05-28 00:28:00 +0900

    update

--- flutter/webtoon/lib/main.dart
+++ flutter/webtoon/lib/main.dart
@@ -10,8 +10,8 @@ class App extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'Flutter Demo',
+    return MaterialApp(
+      home: Container()
     );
   }
 }
