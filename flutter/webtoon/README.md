aade6939966345317d2dff91b150a23ab1baaa18
khkkhk1207 <khkkhk1207@gmail.com>
2023-06-21 15:58:05 +0900

    Data Fetching

--- flutter/webtoon/README.md
+++ flutter/webtoon/README.md
@@ -9,3 +9,9 @@
 ## 패키지 찾기
 
 https://pub.dev/ 라는 Dart, Flutter 공식 패키지 보관소에서 필요한 패키지를 찾는다.
+
+## Dependencies
+
+```shell
+$ flutter pub add http
+```
--- flutter/webtoon/lib/services/api_service.dart
+++ flutter/webtoon/lib/services/api_service.dart
@@ -4,5 +4,13 @@ class ApiService {
   final String baseUrl = "https://webtoon-crawler.nomadcoders.workers.dev";
   final String today = "today";
 
-  void getTodayToons() {}
+  void getTodayToons() async {
+    final url = Uri.parse('$baseUrl/$today');
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      print(response.body);
+      return;
+    }
+    throw Error();
+  }
 }
