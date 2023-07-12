9e617f019c950bdb7a88b0df7a62b12ea67ce9ca
khkkhk1207 <khkkhk1207@gmail.com>
2023-07-12 11:08:34 +0900

    fromJson

--- flutter/webtoon/lib/services/api_service.dart
+++ flutter/webtoon/lib/services/api_service.dart
@@ -1,15 +1,22 @@
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
+import 'package:webtoon/models/webtoon_model.dart';
 
 class ApiService {
   final String baseUrl = "https://webtoon-crawler.nomadcoders.workers.dev";
   final String today = "today";
 
-  void getTodayToons() async {
+  Future<List<WebtoonModel>> getTodayToons() async {
+    List<WebtoonModel> webtoonInstances = [];
     final url = Uri.parse('$baseUrl/$today');
     final response = await http.get(url);
     if (response.statusCode == 200) {
-      print(response.body);
-      return;
+      final List<dynamic> wentoons = jsonDecode(response.body);
+      for (var wentoon in wentoons) {
+        webtoonInstances.add(WebtoonModel.fromJson(wentoon));
+      }
+      return webtoonInstances;
     }
     throw Error();
   }
